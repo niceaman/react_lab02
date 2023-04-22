b29ed0c37b0dd23bdf517bbc55f08d9b5536f014
niceaman <l.salin88@gmail.com>
2023-04-22 22:25:33 +0700

    up: index, main, style

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// Import components
+function App(){
+    return(
+   <>
+   <h1>hello</h1>
+   </>
+    );
+}
+
+// Render
+const domRoot = document.getElementById("root");
+const root = ReactDOM.createRoot(domRoot);
+root.render(<App />);
+
+
